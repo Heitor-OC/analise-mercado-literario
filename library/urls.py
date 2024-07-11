d6dff39990b158347675a6ecd1752687102c71d3
Heitor-OC <132957664+Heitor-OC@users.noreply.github.com>
2024-07-11 20:05:35 -0300

    Update urls.py

--- library/urls.py
+++ library/urls.py
@@ -14,12 +14,13 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
+
 from django.urls import path, include
 from django.views.generic import RedirectView
+from django_mongoengine import mongo_admin
 
 urlpatterns = [
-    path('admin_page/', admin.site.urls),
+    path('admin/', mongo_admin.site.urls),
     path('books/', include('books.urls', namespace='books')),
     path('', RedirectView.as_view(url='/books/', permanent=True)),
 ]
